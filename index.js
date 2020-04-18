e1ae266fd72f2f91eca29651bc34053cade6062b
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-04-18 10:11:44 -0400

    'server'

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // code away!
-const express = require("express")
+const server = require('./server')
 
-const server = express()
+
 const port = 3000
 
-server.use(express.json())
+
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const server = express();
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
