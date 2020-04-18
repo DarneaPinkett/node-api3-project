c3a023f83565a8c9656c0603b2803b79d63bea59
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-04-18 08:57:58 -0400

    index.js P1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+const express = require("express")
+
+const server = express()
+const port = 3000
+
+server.use(express.json())
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
