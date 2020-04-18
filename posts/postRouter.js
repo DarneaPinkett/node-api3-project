ba7d9dbb77f9dca93cc61d470c240293084e2675
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-04-18 10:04:09 -0400

    postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,19 +3,51 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  posts
+  .get()
+  .then(posts => {
+    res.status(201).json(posts);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({message: 'Could not get posts'});
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  posts
+  .getById(id)
+  .then(post => {
+    res.status(201).json(post);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ message: 'Error getting ID'});
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const {id} = req.params;
+  posts
+  .remove(id)
+  .then(post => {
+    res.status(201).json(post);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({message: 'Could not delete post'});
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const {id} = req.params;
+  const response = req.body;
+  posts
+  .update(id, response)
+  .then(response => {
+    console.log(error);
+    res.status(500).json({message: 'Could not update post'});
+  })
 });
 
 // custom middleware
